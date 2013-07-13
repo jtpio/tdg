ce94addd82495cb82bf753c7cfa67f11d9825ac1
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2013-07-13 13:58:28 +0200

    With correct files ...

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-
-var game = require('game.js').game();
+var game = require(__dirname + '/src/game');
 
 server.listen(4444);
 app.use(express.static(__dirname + '/public'));
 
 var players = [];
-
+var games = {};
+var gameCounter = 0;
 
 io.sockets.on('connection', function (client) {
     client.emit("hello", { hello: 'world' });
@@ -19,4 +19,18 @@ io.sockets.on('connection', function (client) {
     });
     client.on('disconnect', function () {
     });
+
+    players.push(client);
+
+    if (players.length % 2 === 0) {
+        var p1 = players.shift();
+        var p2 = players.shift();
+        var newGame = new game(p1, p2, gameCounter);
+        gameCounter++;
+        console.log("new game created");
+        p1.gameID = newGame.id;
+        p2.gameID = newGame.id;
+    }
+
+    console.log("queue size: " + players.length);
 });
--- src/game.js
+++ src/game.js
@@ -1,8 +1,9 @@
 (function() {
 
-    function game(player1, player2) {
+    function game(player1, player2, id) {
         this.player1 = player1;
         this.player2 = player2;
+        this.id = id;
     }
 
     game.prototype = {
@@ -11,6 +12,6 @@
         }
     };
 
-    module.exports.game = game;
+    module.exports = game;
 
 }());
